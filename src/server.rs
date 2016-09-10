360a9e753334b5b7310da6934024d0292989a702
Darin Minamoto <darinm223@gmail.com>
2016-09-09 22:02:45 -0700

    Improved timer code and connection packet handling

--- src/server.rs
+++ src/server.rs
@@ -3,13 +3,15 @@ use mio::*;
 use mio::timer::{Timer, Timeout};
 use mio::udp::UdpSocket;
 
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::SocketAddr;
 use std::str::FromStr;
+use std::time::Duration;
 
+const TIMEOUT_LENGTH: u64 = 5;
 const SERVER: Token = Token(0);
 const TIMER: Token = Token(1);
 
@@ -18,13 +20,14 @@ struct ConnectionState {
     file: File,
     timeout: Timeout,
     block_num: u16,
+    last_packet: Packet,
+    addr: SocketAddr,
 }
 
 pub struct TftpServer {
     new_token: usize,
     poll: Poll,
-    timer: Timer<&'static str>,
-    timeout_queue: VecDeque<Token>,
+    timer: Timer<Token>,
     socket: UdpSocket,
     connections: HashMap<Token, ConnectionState>,
 }
@@ -41,7 +44,6 @@ impl TftpServer {
             new_token: 2,
             poll: poll,
             timer: timer,
-            timeout_queue: VecDeque::new(),
             socket: socket,
             connections: HashMap::new(),
         })
@@ -55,10 +57,6 @@ impl TftpServer {
         let token = Token(self.new_token);
         self.new_token += 1;
         token
-    }
-
-    fn register_timeout(&mut self) -> io::Result<Timeout> {
-        unimplemented!()
     }
 
     fn handle_server_packet(&mut self) -> io::Result<bool> {
@@ -85,7 +83,8 @@ impl TftpServer {
 
         let mut file: File;
         let block_num: u16;
-        let timeout: Timeout;
+        let timeout = self.timer.set_timeout(Duration::from_secs(TIMEOUT_LENGTH), token).unwrap();
+        let last_packet: Packet;
         // Handle the RRQ or WRQ packet
         match packet {
             Packet::RRQ { filename, mode } => {
@@ -96,26 +95,23 @@ impl TftpServer {
                 try!(file.read(&mut buf));
 
                 // Reply with first data packet with a block number of 1
-                let data_packet = Packet::DATA {
+                last_packet = Packet::DATA {
                     block_num: block_num,
                     data: DataBytes(buf),
                 };
-                let data_bytes = try!(data_packet.bytes());
-                socket.send_to(&data_bytes[..], &src);
-                timeout = try!(self.register_timeout()); // TODO(DarinM223): fix this
             }
             Packet::WRQ { filename, mode } => {
                 file = try!(File::create(filename));
                 block_num = 0;
 
                 // Reply with ACK with a block number of 0
-                let ack_packet = Packet::ACK(block_num);
-                let ack_bytes = try!(ack_packet.bytes());
-                socket.send_to(&ack_bytes[..], &src);
-                timeout = try!(self.register_timeout()); // TODO(DarinM223): fix this
+                last_packet = Packet::ACK(block_num);
             }
             _ => unreachable!(),
         }
+
+        let packet_bytes = try!(last_packet.clone().bytes());
+        try!(socket.send_to(&packet_bytes[..], &src));
 
         self.connections.insert(token,
                                 ConnectionState {
@@ -123,37 +119,73 @@ impl TftpServer {
                                     file: file,
                                     timeout: timeout,
                                     block_num: block_num,
+                                    last_packet: last_packet,
+                                    addr: src,
                                 });
 
         Ok(false)
     }
 
     fn handle_timer(&mut self) -> io::Result<bool> {
-        // TODO(DarinM223): dequeue token from queue
-        // TODO(DarinM223): resend last data packet
-        unimplemented!()
+        let token = self.timer.poll().unwrap();
+        if let Some(ref mut conn) = self.connections.get_mut(&token) {
+            let last_packet = conn.last_packet.clone();
+            let last_packet_bytes = try!(last_packet.bytes());
+            try!(conn.conn.send_to(&last_packet_bytes[..], &conn.addr));
+        }
+
+        Ok(false)
     }
 
     fn handle_connection_packet(&mut self, token: Token) -> io::Result<bool> {
-        if let Some(ref mut conn_state) = self.connections.get_mut(&token) {
+        if let Some(ref mut conn) = self.connections.get_mut(&token) {
             let mut buf = [0; MAX_PACKET_SIZE];
-            let (_, src) = try!(conn_state.conn.recv_from(&mut buf)).unwrap();
+            let (_, src) = try!(conn.conn.recv_from(&mut buf)).unwrap();
             let packet = try!(Packet::read(buf));
 
             match packet {
                 Packet::ACK(block_num) => {
-                    // TODO(DarinM223): check if block num equals the connection's block num
-                    // TODO(DarinM223): if true, send next packet and reset timeout
+                    if block_num != conn.block_num {
+                        // TODO(DarinM223): handle error
+                        panic!("Invalid block number received");
+                    }
+
+                    conn.block_num += 1;
+                    let mut buf = [0; 512];
+                    try!(conn.file.read(&mut buf));
+
+                    // Send next data packet
+                    let packet = Packet::DATA {
+                        block_num: conn.block_num,
+                        data: DataBytes(buf),
+                    };
+                    let packet_bytes = try!(packet.bytes());
+                    try!(conn.conn.send_to(&packet_bytes[..], &conn.addr));
                 }
-                Packet::DATA { .. } => {
-                    // TODO(DarinM223): write data to file
-                    // TODO(DarinM223): send ACK packet
+                Packet::DATA { block_num, data } => {
+                    if block_num != conn.block_num + 1 {
+                        // TODO(DarinM223): handle error
+                        panic!("Invalid block number received");
+                    }
+
+                    conn.block_num += 1;
+                    try!(conn.file.write(&data.0[..]));
+
+                    // Send ACK packet for data
+                    let packet = Packet::ACK(conn.block_num);
+                    let packet_bytes = try!(packet.bytes());
+                    try!(conn.conn.send_to(&packet_bytes[..], &conn.addr));
                 }
                 Packet::ERROR { .. } => {
                     // TODO(DarinM223): terminate connection
+                    panic!("Error!");
                 }
                 _ => {}
             }
+
+            // Reset timeout
+            self.timer.cancel_timeout(&conn.timeout);
+            self.timer.set_timeout(Duration::from_secs(TIMEOUT_LENGTH), token);
         }
 
         Ok(false)
