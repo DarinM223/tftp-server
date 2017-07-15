94681e4f736588b1ec81cb256a708383ce56e917
Alexandru Macovei <alexnmaco@gmail.com>
2017-07-15 12:42:47 +0300

    clap test

--- src/bin.rs
+++ src/bin.rs
@@ -1,5 +1,7 @@
 extern crate env_logger;
 extern crate tftp_server;
+
+#[macro_use]
 extern crate clap;
 
 use tftp_server::server::TftpServer;
@@ -7,8 +9,33 @@ use std::env;
 use std::str::FromStr;
 use std::net::SocketAddr;
 
+use clap::{Arg, App};
+
 fn main() {
     env_logger::init().unwrap();
+
+    let matches = App::new("My Super Program")
+                          .version(crate_version!())
+                          .about("A TFTP server")
+                          .arg(Arg::with_name("directory")
+                               .short("d")
+                               .long("directory")
+                               .value_name("DIR")
+                               .help("serves the specified directory instead of the current one")
+                               .takes_value(true))
+                          .arg(Arg::with_name("readonly")
+                               .short("r")
+                               .long("readonly")
+                               .help("rejects all write requests to the served directory"))
+                          .arg(Arg::with_name("IPv4")
+                               .short("4")
+                               .long("ipv4")
+                               .help("enables IPv4"))
+                          .arg(Arg::with_name("IPv6")
+                               .short("6")
+                               .long("ipv6")
+                               .help("enables IPv6"))
+                          .get_matches();
 
     let args: Vec<_> = env::args().collect();
     let mut server: TftpServer;
